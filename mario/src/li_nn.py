Fixed mario MDP settings so that it rewards for coins. Made experience not so unlikely to remember. Fixed li_nn handling of single output backpropagation based on errors that came up when it actually ran.

mario/src/li_nn.py
@@ -64,7 +64,7 @@ class NeuralNet:
             p2 = np.multiply(activation[j], (1-activation[j]))
             error[j]= np.multiply(p1,p2)
         
-        error[Y==0.0] == 0.0 #IMPORTANT: This step makes us only update the action that we observed   
+        error[length-1][Y==0.0] == 0.0 #IMPORTANT: This step makes us only update the action that we observed   
         
         # calculate gradient
         if grad[length-2] == None: